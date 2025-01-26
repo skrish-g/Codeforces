89149732301d7f7ab0810b341bf5d9f80aff6eba
skrish-g <skrapmp166@gmail.com>
2025-01-26 22:43:47 +0530

    Launching Codeforces

---
+++ 800 Rating/Line Trip.c
@@ -0,0 +1,16 @@
+// Thank You Zabi Sir!
+#include <stdio.h>
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    for(int i=1; i<=t; i++)
+    {
+        int n, x;
+        scanf("%d %d", &n, &x);
+        for(int j=1; j<=n; j++)
+        {
+            
+        }
+    }
+}
