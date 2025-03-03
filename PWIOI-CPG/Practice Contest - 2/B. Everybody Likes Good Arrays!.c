4b5351edb8598c61308e0a08fb147ac506f305d1
Krish Gupta <skrapmp166@gmail.com>
2025-03-03 22:23:58 +0530

    Problem-B.

---
+++ PWIOI-CPG/Practice Contest - 2/B. Everybody Likes Good Arrays!.c
@@ -0,0 +1,21 @@
+// Thank You Zabi Sir!
+#include <stdio.h>
+int main()
+{
+    int T;
+    scanf("%d", &T);
+    for(int i=0; i<T; i++)
+    {
+        int count=0;
+        int n;
+        scanf("%d", &n);
+        int a[n];
+        for(int i=0; i<n; i++)
+        {
+            scanf("%d ", &a[i]);
+            if(a[i]%2==a[i-1]%2 && i!=0)
+                count++;
+        }
+        printf("%d\n", count);
+    }
+}
