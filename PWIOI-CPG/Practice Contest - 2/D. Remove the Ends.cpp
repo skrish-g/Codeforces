244da39082791014b40d9f3fd68236c679b78145
Krish Gupta <skrapmp166@gmail.com>
2025-06-20 01:17:52 +0530

    Updation

--- PWIOI-CPG/Practice Contest - 2/D. Remove the Ends.cpp
+++ PWIOI-CPG/Practice Contest - 2/D. Remove the Ends.cpp
@@ -12,30 +12,28 @@ int main()
         vector<int>a(n,0);
         for(int i=0; i<n; i++)
             cin>>a[i];
-        long pf_pos[n];
-        long pf_neg[n];
-        long sum=0;
+        long long pf_pos[n];
+        long long pf_neg[n];
+        long long sum=0;
         for(int i=0; i<n; i++)
-        {
-            if(a[i]<0)
-                sum-=a[i];
-            pf_neg[i]=sum;
-        }
-        sum=0;
-        for(int i=n-1; i>=0; i--)
         {
             if(a[i]>0)
                 sum+=a[i];
             pf_pos[i]=sum;
         }
-        int ans=0;
+        sum=0;
         for(int i=n-1; i>=0; i--)
         {
-            if(a[i]>0 && pf_neg[i]>a[i])
-            {
-                ans+=pf_neg[i];
-            }
-            else if(a[i]<0 && pf_pos[i]>a[i])
+            if(a[i]<0)
+                sum-=a[i];
+            pf_neg[i]=sum;
         }
+        long long ans=0;
+        for(int i=0; i<n; i++)
+        {
+            if((pf_pos[i]+pf_neg[i])>ans)
+                ans=pf_pos[i]+pf_neg[i];
+        }
+        cout<<ans<<"\n";
     }
 }
